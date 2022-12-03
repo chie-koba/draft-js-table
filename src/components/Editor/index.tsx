908997751f206d3ca863806cd9ec2b6664167cba
chie-koba <akitubaki.ck@gmail.com>
2022-12-04 01:48:59 +0900

    fix: next.jsでdraft.jsが利用できるように修正

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,12 +1,38 @@
 import React, { useCallback, useState } from "react";
-import { Editor as DraftJsEditor, EditorState } from "draft-js";
+import {
+  convertFromRaw,
+  Editor as DraftJsEditor,
+  EditorState,
+  genKey,
+} from "draft-js";
 
 export const Editor: React.FC = () => {
-  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  // see: https://github.com/facebook/draft-js/issues/2332
+  const [editorState, setEditorState] = useState(
+    EditorState.createWithContent(
+      convertFromRaw({
+        entityMap: {},
+        blocks: [
+          {
+            text: "",
+            key: genKey(),
+            type: "unstyled",
+            entityRanges: [],
+            depth: 0,
+            inlineStyleRanges: [],
+          },
+        ],
+      })
+    )
+  );
 
   const onChange = useCallback((state: EditorState) => {
     setEditorState(state);
   }, []);
 
-  return <DraftJsEditor editorState={editorState} onChange={onChange} />;
+  return (
+    <div>
+      <DraftJsEditor editorState={editorState} onChange={onChange} />
+    </div>
+  );
 };
